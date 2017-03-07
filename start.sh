fa6b4f1b4b4f78e659cbd025d2674d7f47ce515f
Pēteris Caune <cuu508@gmail.com>
2017-03-07 20:05:33 +0200

    Use dumb-init, don't use gosu, insall postgres, mysql and uwsgi stuff with apt-get instead of pip

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-set -e
+set -e -x
 
-gosu hc uwsgi --master \
+su -c "python3 /src/manage.py migrate" hc
+
+uwsgi --master \
+    --uid hc \
+    --gid hc \
     --http-socket :9090 \
-    --processes 4 \
+    --processes 2 \
+    --threads 2 \
     --chdir /src \
+    --plugin python3 \
     --module hc.wsgi:application \
     --enable-threads \
     --thunder-lock \
     --static-map /static=/src/static-collected \
-    --attach-daemon "/src/manage.py sendalerts"
+    --attach-daemon "python3 manage.py sendalerts"
